6b1922cab53006ce7d9b119a2b9ea1143b790035
claresinger <csinger@caltech.edu>
2020-11-03 16:54:38 -0800

    notebook 6

--- src/ESE156_2020.jl
+++ src/ESE156_2020.jl
@@ -1,5 +1,5 @@
 using IJulia
 using Pkg.Artifacts
-""" Shorthand for @artifact_str """
-artifact(name) = joinpath(@artifact_str(name), name)
+#""" Shorthand for @artifact_str """
+#artifact(name) = joinpath(@artifact_str(name), name)
 jupyterlab(dir=joinpath(dirname(@__FILE__), ".."))
